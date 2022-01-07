0d66e07ce3a3e8ae7e6813b19b87e1d9ce25fb20
Seyoung Hong <maiorem00@gmail.com>
2022-01-07 22:14:08 +0900

    auth

--- src/main/java/com/maiorem/project/config/SecurityConfig.java
+++ src/main/java/com/maiorem/project/config/SecurityConfig.java
@@ -1,4 +1,30 @@
 package com.maiorem.project.config;
 
-public class SecurityConfig {
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // 사용자 계정 user1
+        auth.inMemoryAuthentication().withUser("user1")
+                // password 1111 인코딩 결과
+                .password("$2a$10$FCMJNNaiKdKziZppY4hFN.Soq.9hIA.GJ1XbVxijyGzXPJD1Jo68e")
+                .roles("USER");
+    }
 }
---
+++ src/main/java/com/maiorem/project/controller/SampleController.java
@@ -0,0 +1,28 @@
+package com.maiorem.project.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/sample")
+public class SampleController {
+
+    @GetMapping("/all")
+    public void exAll(){
+        log.info("exAll.................");
+    }
+
+    @GetMapping("/member")
+    public void exMember(){
+        log.info("exMember.................");
+    }
+
+    @GetMapping("/admin")
+    public void exAdmin() {
+        log.info("exAdmin....................");
+    }
+
+}
